51727b69b4a52cc42acd71f215c5588524351880
Max Mekenya <dj.cjmaxik@gmail.com>
2018-01-08 12:29:09 +1000

    Remove unnesessary "profile" injection (postponed to the next version)

--- src/inject/profile/inject.js
+++
@@ -1,6 +0,0 @@
-$('body > div.wrapper > div.container.content.profile > div > div.col-md-9 > div > div:nth-child(4)').each(function () {
-  var str = $(this).html();
-  var regex = /((http|https):\/\/[\w?=&.\/-;#~%-]+(?![\w\s?&.\/;#~%"=-]*>))/gi;
-  var replaced_text = str.replace(regex, '<a href="$1" target="_blank" rel="noreferrer noopener" class="replaced">$1</a>');
-  $(this).html(replaced_text);
-});
